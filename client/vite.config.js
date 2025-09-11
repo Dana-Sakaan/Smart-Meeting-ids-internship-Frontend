751c99c1b01ae9b191ed0a3110859feb35494c70
Dana <danasakaan4@gmail.com>
2025-09-12 00:26:08 +0300

    add NEW ENVV VARIABLE

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
